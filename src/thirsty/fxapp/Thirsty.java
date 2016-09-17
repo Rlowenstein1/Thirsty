fdc27976a6c59cd4e2086eebd9f5c7a8aeb4e92e
Tyler Brown <xtoottootx222@gmail.com>
2016-09-16 22:06:44 -0400

    Setup basic structure of program.

---
+++ src/thirsty/fxapp/Thirsty.java
@@ -0,0 +1,28 @@
+package thirsty.fxapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Thirsty extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginScreen.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
