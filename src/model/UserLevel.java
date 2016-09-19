ba404cdd2bd8f5bead35c384514a6ef6b3cbc0a9
Kenny Scharm <kscharm3@gatech.edu>
2016-09-19 12:53:59 -0400

    Added getValue and fromInt to UserLevel class

--- src/model/UserLevel.java
+++ src/model/UserLevel.java
@@ -8,4 +8,19 @@ public enum UserLevel {
     UserLevel(int level) {
         this.level = level;
     }
+
+    // Access int value
+    public int getValue() {
+        return level;
+    }
+
+    // Get UserLevel enum when you only know the int value
+    public static UserLevel fromInt(int i) {
+        for (UserLevel l: UserLevel.values()) {
+            if (l.getValue() == i) {
+                return l;
+            }
+        }
+        return null;
+    }
 }
