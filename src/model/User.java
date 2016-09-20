31ec24d56a67a8f46aef8fb3e753cabb4c0de943
Kenny Scharm <kscharm3@gatech.edu>
2016-09-19 20:59:09 -0400

    Changed User.java to include object properties

--- src/model/User.java
+++ src/model/User.java
@@ -1,58 +1,64 @@
 package model;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class User {
-    private String name;
-    private String username;
-    private String emailAddress;
-    private UserLevel level;
+    private final StringProperty name = new SimpleStringProperty();
+    private final StringProperty username = new SimpleStringProperty();
+    private final StringProperty emailAddress = new SimpleStringProperty();
+    private final ObjectProperty<UserLevel> level = new SimpleObjectProperty();
+
 
     public User(String username) {
-        this(username, "Name", "name.gmail.com", UserLevel.USER);
+        this(username, "Name", "sample.gmail.com", UserLevel.USER);
     }
 
     public User(String username, String name, String emailAddress, UserLevel level) {
-        this.username = username;
-        this.name = name;
-        this.emailAddress = emailAddress;
-        this.level = level;
+        this.username.set(username);
+        this.name.set(name);
+        this.emailAddress.set(emailAddress);
+        this.level.set(level);
     }
 
     public String getName() {
-        return name;
+        return name.get();
     }
 
     public void setName(String n) {
-        name = n;
+        name.set(n);
     }
 
     public String getUsername() {
-        return username;
+        return username.get();
     }
 
     public void setUsername(String n) {
-        username = n;
+        username.set(n);
     }
 
     public String getEmailAddress() {
-        return emailAddress;
+        return emailAddress.get();
     }
 
     public void setEmailAddress(String n) {
-        emailAddress = n;
+        emailAddress.set(n);
     }
 
     public UserLevel getUserLevel() {
-        return level;
+        return level.get();
     }
 
     public void setUserLevel(UserLevel l) {
-        level = l;
+        level.set(l);
     }
 
     public String toString() {
-        return "Name: " + name + "\n" +
-                "Username: " + username + "\n" +
-                "Email address: " + emailAddress + "\n" +
-                "Authorization level: " + level;
+        return "Name: " + name.get() + "\n" +
+                "Username: " + username.get() + "\n" +
+                "Email address: " + emailAddress.get() + "\n" +
+                "Authorization level: " + level.get();
     }
 }
