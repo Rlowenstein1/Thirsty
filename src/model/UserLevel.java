8c06615f68d25a9167fe23c73a370d685cd2e13a
Kenny Scharm <kscharm3@gatech.edu>
2016-09-19 21:20:16 -0400

    Added Javadocs to all methods and added one more constructor to User.java

--- src/model/UserLevel.java
+++ src/model/UserLevel.java
@@ -1,20 +1,37 @@
 package model;
 
+/**
+ * An enum that represents the four different authority levels of Users
+ */
 public enum UserLevel {
     USER(1), WORKER(2), MANAGER(3), ADMINISTRATOR(4);
 
     private final int level;
 
+    /**
+     * The default constructor for the UserLevel enum
+     *
+     * @param level the integer value for the authority level
+     */
     UserLevel(int level) {
         this.level = level;
     }
 
-    // Access int value
+    /**
+     * Gets the integer value of a UserLevel
+     *
+     * @return the integer (1-4) representing a User's authority level
+     */
     public int getValue() {
         return level;
     }
 
-    // Get UserLevel enum when you only know the int value
+    /**
+     * Gets UserLevel enum from an integer value(1-4)
+     *
+     * @param i the integer value of the UserLevel
+     * @return a UserLevel object
+     */
     public static UserLevel fromInt(int i) {
         for (UserLevel l: UserLevel.values()) {
             if (l.getValue() == i) {
