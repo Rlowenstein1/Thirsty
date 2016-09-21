91c69eb5ad0f95667c924d757ec5682c9d46ef09
Tyler Brown <xtoottootx222@gmail.com>
2016-09-20 20:41:04 -0400

    Implemented handleLoginButtonAction, and basic handling for register button

--- src/controller/SplashScreenController.java
+++ src/controller/SplashScreenController.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import lib.Debug;
 
 /**
  *
  * @author tybrown
  */
-public class SplashScreenController {
-    
+public class SplashScreenController implements Initializable {
+    @FXML
+    public void handleLoginButtonAction() {
+        Debug.debug("Login button pressed");
+        boolean loggedIn = MasterSingleton.showLoginScreen();
+        if (loggedIn) {
+            Debug.debug("User logged in!");
+            MasterSingleton.showMainScreen();
+        } else {
+            Debug.debug("User cancelled login");
+        }
+    }
+
+    @FXML
+    public void handleRegisterButtonAction() {
+        Debug.debug("Registration button pressed");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+   
 }
