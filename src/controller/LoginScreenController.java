0f1f45fcf6742ac2a73b61568d9f0222867df3f0
Tyler Brown <xtoottootx222@gmail.com>
2016-09-20 20:39:57 -0400

    Added ability to check if login was successful

--- src/controller/LoginScreenController.java
+++ src/controller/LoginScreenController.java
@@ -6,21 +6,48 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import lib.Debug;
+import model.Authenticator;
+import model.User;
 
 public class LoginScreenController implements Initializable {
+
+    private Stage stage;
+    private boolean authed = false;
+
+    @FXML
+    private TextField usernameField;
     
     @FXML
-    private Label label;
+    private PasswordField pwField;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleLoginButtonAction(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = pwField.getText();
+        Debug.debug("Verifying user credentials: \"%s\":\"%s\"", username, password);
+        User authenticated = Authenticator.authenticate(username, password);
+        if (authenticated != null) {
+            Debug.debug("User authentication successful!");
+            authed = true;
+        } else {
+            Debug.debug("User authentication failed!");
+        }
     }
     
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public boolean loginSuccessful() {
+        return (authed);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
     }    
     
 }
