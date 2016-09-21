ee293cec62407a0c5708b50cccde81004efc0e8b
Tyler Brown <xtoottootx222@gmail.com>
2016-09-20 20:41:39 -0400

    Implemented basic logout button handling

--- src/controller/MainScreenController.java
+++ src/controller/MainScreenController.java
@@ -1,18 +1,32 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 import lib.Debug;
 
 /**
  *
  * @author tybrown
  */
-public class MainScreenController {
+public class MainScreenController implements Initializable {
 
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
 
     @FXML
     private void handleLogoutButtonAction(ActionEvent event) {
+        MasterSingleton.showSplashScreen();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
     }
     
 }
