85d8cb4ddec27d6aea5649c849bd214952bcbf62
Tyler Brown <xtoottootx222@gmail.com>
2016-09-20 23:11:32 -0400

    Added basic registration validation

--- src/controller/RegistrationScreenController.java
+++ src/controller/RegistrationScreenController.java
@@ -2,10 +2,19 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import lib.Debug;
+import model.Authenticator;
+import model.User;
+import model.UserLevel;
 
 /**
  *
@@ -14,13 +23,76 @@ import lib.Debug;
 public class RegistrationScreenController implements Initializable {
 
     private Stage stage;
+    private boolean registered = false;
+    private User newReg = null;
 
+    @FXML
+    private TextField usernameRegField;
+    
+    @FXML
+    private PasswordField pwRegField;
+ 
+    @FXML
+    private PasswordField pwConfRegField;
+ 
+    @FXML
+    private TextField emailRegField;
+    
+    @FXML
+    private TextField fullnameRegField;
+    
+    @FXML
+    private ChoiceBox<UserLevel> accountTypeRegBox;
+    
+    @FXML
+    private void handleLoginButtonAction(ActionEvent event) {
+        String fullname = fullnameRegField.getText();
+        String username = usernameRegField.getText();
+        String email = emailRegField.getText();
+        UserLevel userLevel = accountTypeRegBox.getValue();
+        String password = pwRegField.getText();
+        String passwordConf = pwConfRegField.getText();
+        Debug.debug("Attempting to register user: username: \"%s\"; fullname: \"%s\"; email: \"%s\"; password: \"%s\"; passwordConf: \"%s\"; type: \"%s\"", username, fullname, email, password, passwordConf, userLevel.toString());
+        if (fullname.length() == 0) {
+            Debug.debug("Fullname field cannot be left blank!");
+        } else if (username.length() == 0) {
+            Debug.debug("Username field cannot be left blank!");
+        } else if (email.length() == 0) {
+            Debug.debug("email field cannot be left blank!");
+        } else if (password.length() == 0) {
+            Debug.debug("Password field cannot be left blank!");
+        } else if (passwordConf.length() == 0) {
+            Debug.debug("Password confirmation field cannot be left blank!");
+        } else {
+            if (Authenticator.getUser(username) != null) {
+                Debug.debug("Username already taken!");
+            } else if (!password.equals(passwordConf)) {
+                Debug.debug("Password and password confirmation do not match!");
+            } else {
+                Debug.debug("User registration successful!");
+                newReg = Authenticator.register(username, fullname, email, password, userLevel);
+                registered = true;
+                stage.close();
+            }
+        }
+        Debug.debug("User registration failed!");
+    }
+    
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
+    public boolean registrationSuccessful() {
+        return (registered);
+    }
+
+    public User getNewUser() {
+        return (newReg);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    }
+        accountTypeRegBox.setItems(UserLevel.getAllObservableList());
+    }    
     
 }
