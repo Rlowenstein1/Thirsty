0c7603d0ba50303a0a7bec811824e8aa55b19342
jtpeterson <jpeterson@gatech.edu>
2016-09-30 00:23:38 -0400

    Updated Tests

--- src/ThirstyTester.java
+++ src/ThirstyTester.java
@@ -3,11 +3,14 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
+import junit.framework.Assert;
 import model.UserLevel;
 import model.UserManager;
-import model. Authenticator;
+import model.Authenticator;
+import model.User;
 import org.junit.Before;
 import org.junit.Test;
+import org.omg.CORBA.TIMEOUT;
 
 public class ThirstyTester {
     public static final int TIMEOUT = 200;
@@ -24,7 +27,38 @@ public class ThirstyTester {
     @Test(timeout = TIMEOUT)
     public void testAuthenticatorRegisterFailure() {
         assertFalse(Authenticator.register("user", "1234"));
-        Authenticator.register("new", "1");
+        assertTrue(Authenticator.register("new", "1"));
         assertFalse(Authenticator.register("new", "2"));
+    }
+
+    @Test(timeout = TIMEOUT, expected = IllegalArgumentException.class)
+    public void testAuthenticatorRegisterUserIllegalArg() {
+        Authenticator.register(null, "1");
+    }
+
+    @Test(timeout = TIMEOUT, expected = IllegalArgumentException.class)
+    public void testAuthenticatorRegisterPasswordIllegalArg() {
+        Authenticator.register("newUser2", null);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testUserLoginTrue() {
+        UserManager.register("newUser3", "newPass", "Name", "Email", UserLevel.USER);
+        User test = UserManager.login("newUser3", "newPass");
+        assertTrue(test != null);
+    }
+    /* Possibly Irrelevant
+    @Test(timeout = TIMEOUT)
+    public void testUserLoginTrue2() {
+        Authenticator.register("newUser4", "newPass2");
+        User test = UserManager.login("newUser4", "newPass2");
+        assertFalse(test == null);
+    }
+    */
+    @Test(timeout = TIMEOUT)
+    public void testUserLoginFalse() {
+        UserManager.register("newUser5", "newPass3", "Name", "Email", UserLevel.USER);
+        User test = UserManager.login("newUser5", "newPass300000");
+        assertTrue(test == null);
     }
 }
