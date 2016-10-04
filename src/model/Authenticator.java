afa58dcced24d667bc7ab6353165b4f5292a0990
jtpeterson <jpeterson@gatech.edu>
2016-10-03 21:33:59 -0400

    updated checkstyle

--- src/model/Authenticator.java
+++ src/model/Authenticator.java
@@ -11,7 +11,7 @@ public final class Authenticator {
     /**
      * singleton instance.
      */
-    public static Authenticator authenticator = new Authenticator();
+    private static Authenticator authenticator = new Authenticator();
 
     /**
      * hash table with the users username and password hashes.
